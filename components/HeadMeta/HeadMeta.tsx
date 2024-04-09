b0974a184b64ab74606164ec76a1f2b4af8a2d04
openservicedv <150409269+openservicedv@users.noreply.github.com>
2024-04-08 18:58:41 -0700

    1

--- components/HeadMeta/HeadMeta.tsx
+++ components/HeadMeta/HeadMeta.tsx
@@ -1,12 +1,9 @@
-import Image from "next/image";
-import styles from "./CharacterCard.module.css";
 import Head from "next/head";
 
 type PropsType = {
     title: string
     description?: string
 }
-
 export const HeadMeta = (props: PropsType) => {
     const {title, description} = props
 
