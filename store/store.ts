7d0a12a6dc6ac62078a17a2791d8819701b7f960
openservicedv <150409269+openservicedv@users.noreply.github.com>
2024-05-01 12:04:13 -0700

    apply RTK Query

--- store/store.ts
+++ store/store.ts
@@ -1,11 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { charactersApi } from '@/assets/hooks/useCharacters'
 
 export const store = configureStore({
     reducer: {
-        // character: characterReducer,
-        // characters: charactersReducer,
-    }
+        [charactersApi.reducerPath]: charactersApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(charactersApi.middleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
+
+setupListeners(store.dispatch)
